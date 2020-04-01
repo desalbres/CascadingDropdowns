841a78248c86a82d5db068eb113c322cd9fef3b1
Raphaël Désalbres <raphael.desalbres@outlook.com>
2020-04-01 23:03:29 +0200

    Initial Commit

---
+++ CascadingDropdowns/Data/ProductService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CascadingDropdowns.Data
+{
+    public class ProductService
+    {
+        public List<Products> getProducts
+        {
+            get {
+                return new List<Products> {
+                    new Products { Name="Cars",  Brands= new List<string>() {"Volkswagen","Ford","Toyota","Fiat" } },
+                    new Products { Name="Motorcycles",  Brands= new List<string>() {"Kawasaki","Yamaha","BMW" }},
+                    new Products { Name="Planes",  Brands= new List<string>() {"Airbus","Boieng","ATR" }}
+                };
+            } 
+        }
+    }
+}
---
+++ CascadingDropdowns/Data/Products.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CascadingDropdowns.Data
+{
+    public class Products
+    {
+        public string Name { get; set; }
+        public List<string> Brands { get; set; }
+    }
+}
